3b21791d9cf46a25c40f778abb62face1c19c0ad
nishanth <varmanishanth90@gmail.com>
2026-06-08 20:14:22 +0530

    port 5000 added in app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 from db import get_news
 from nlp_utils import process_text, summarize_text, categorize
@@ -59,4 +60,5 @@ def news_search():
     return render_template("index.html", results=results, query=query, error=error)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
